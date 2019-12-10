304b1375a0d9d8f81f59fa7b0e09d76e3e35fefc
244267620@qq.com <wlc68911qet>
2019-12-10 14:25:53 +0800

    update

--- extract/lang.py
+++ extract/lang.py
@@ -12,11 +12,11 @@ def extract_char_lang(char_dict, file_path):
     """
     file_list = os.listdir(file_path)
     for file in file_list:
-        path = os.path.join(file_path, file)
-        if os.path.isfile(path):
-            with open(path, 'r', encoding='utf-8') as file_object:
-                for line in file_object:
-                    for c in line:
-                        char_dict[c] = 1
-        else:
-            extract_char_lang(char_dict, path)
+	    path = os.path.join(file_path, file)
+	    if os.path.isfile(path):
+		    with open(path, 'r', encoding='utf-8') as file_object:
+			    for line in file_object:
+				    for c in line.rstrip('\n'):
+					    char_dict[c] = 1
+	    else:
+		    extract_char_lang(char_dict, path)
